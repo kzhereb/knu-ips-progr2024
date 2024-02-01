5364e3f10e3146cd7c690ffed7a78e80a62c86bd
Kostiantyn Zhereb <zhereb@gmail.com>
2024-02-01 15:28:56 +0200

    Progress commit - seems to work, but needs refactoring

--- src/lesson_2024_02_01_linkedlist.cpp
+++ src/lesson_2024_02_01_linkedlist.cpp
@@ -8,7 +8,38 @@
 #include <iostream>
 
 
+struct Node {
+  int value;
+  Node* prev;
+  Node* next;
+};
+
 int main() {
+  Node* start = new Node;
+  Node* end = new Node;
+
+  // create list  [1, 3, 5]
+
+  start->value = 1;
+  //start->prev = new Node;
+  start->prev = nullptr;
+  start->next = new Node;
+  start->next->value = 3;
+  start->next->prev = start;
+  start->next->next = end;
+ // end->prev = new Node;
+  //end->next = new Node;
+  end->value = 5;
+  end->prev = start->next;
+  end->next = nullptr;
+
+  std::cout<<"middle from start " << start->next->value<<"\n";
+  std::cout<<"middle from end " << end->prev->value<<std::endl;
+  return 0;
+
+}
+
+int hello() {
   std::cout << "hello world!\n"; // doesn't flush buffer to stdout - can miss message
   std::cout.flush(); // forces buffer to stdout
   //std::cout << "hello world (again)!" << std::endl; //std::endl also flushes buffer
