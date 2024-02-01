56febbeb1ec7444bda978f8fab22d89adc109f2d
Kostiantyn Zhereb <zhereb@gmail.com>
2024-02-01 14:46:45 +0200

    Added hello world code to test IDE
    Check that it compiles and runs C++ code
    Also updated .gitignore to support Eclipse-specific files

---
+++ src/lesson_2024_02_01_linkedlist.cpp
@@ -0,0 +1,17 @@
+/*
+ * lesson_2024_02_01_linkedlist.cpp
+ *
+ *  Created on: Feb 1, 2024
+ *      Author: KZ
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "hello world!";
+  return 0;
+}
+
+
