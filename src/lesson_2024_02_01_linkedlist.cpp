e6f5526806abbd945bd7dfb043fe011e4cb9e543
Kostiantyn Zhereb <zhereb@gmail.com>
2024-02-01 15:04:08 +0200

    Demonstrates difference between std::endl and \n
    Need to flush buffer so that output appears in console
    This is done automatically by std::endl, or can be done manually by
    calling flush()
    Without it, some messages may not show, which may lead to hard-to-debug
    situations

--- src/lesson_2024_02_01_linkedlist.cpp
+++ src/lesson_2024_02_01_linkedlist.cpp
@@ -9,7 +9,10 @@
 
 
 int main() {
-  std::cout << "hello world!";
+  std::cout << "hello world!\n"; // doesn't flush buffer to stdout - can miss message
+  std::cout.flush(); // forces buffer to stdout
+  //std::cout << "hello world (again)!" << std::endl; //std::endl also flushes buffer
+  while(true) { } // endless loop - to show possible issues when buffer is not flushed
   return 0;
 }
 
