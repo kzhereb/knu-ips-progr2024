cc63cb17b3250835572acbb02dec072abd85659c
Kostiantyn Zhereb <zhereb@gmail.com>
2024-03-07 15:38:47 +0200

    Progress commit - started implementing second step (min_in_block)
    Need to implement code for finding minimal item (and its index) in array

--- src/lesson_2024_03_07_sorting.cpp
+++ src/lesson_2024_03_07_sorting.cpp
@@ -6,6 +6,8 @@
  */
 
 #include <iostream>
+#include <cmath>
+#include <cassert>
 
 namespace lesson_2024_03_07_sorting {
 
@@ -28,6 +30,19 @@ void print_array(int* array, size_t size, size_t items_to_print = 5) {
 
 int* square_selection_sort(int* array, size_t size) {
   int* sorted = new int[size];
+  size_t block_size = std::sqrt(size); // |B_i| in slides
+  size_t num_blocks = std::sqrt(size); // m in slides
+  assert( size == num_blocks*block_size); //TODO: change it, in general case not true
+  int* min_in_block = new int[num_blocks]; // g_i in slides
+
+  for (size_t i = 0; i < num_blocks; i++) {
+    //int min_value = ???
+    for(size_t j = i*block_size; j < (i+1)*block_size; j++) {
+
+    }
+  }
+
+
   //print_array(sorted, size);
   return sorted;
 }
