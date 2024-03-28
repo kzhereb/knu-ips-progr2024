afa0a7679f5133f676cfc9b25f2c37b43652601b
Kostiantyn Zhereb <zhereb@gmail.com>
2024-03-28 13:56:10 +0200

    Another implementation of algorithm without copying input array
    We don't need to remove min elements from input array - just pass
    current min and ignore all previous min when finding next min

--- src/lesson_2024_03_07_sorting.cpp
+++ src/lesson_2024_03_07_sorting.cpp
@@ -62,6 +62,24 @@ int find_min(int *arr, size_t arr_size, size_t &index) {
 
   return min_value;
 }
+
+// finds next min element, not including current min (and all previous min elements)
+int find_next_min(int *arr, size_t arr_size, int current_min, size_t current_min_index, size_t &index) {
+  int min_value = INT_MAX;//arr[0];
+  index = 0; // will be replaced
+
+  for (size_t i = 0; i < arr_size; ++i) {
+    if (arr[i] < current_min) { continue;}
+    if (arr[i] == current_min && i <= current_min_index) { continue;}
+    if (arr[i] < min_value) {
+      min_value = arr[i];
+      index = i;
+    }
+  }
+
+  return min_value;
+}
+
 // finds min value in subarray from start (inclusive) to end (inclusive)
 int find_min_subarray(int *arr, size_t start, size_t end, size_t &index) {
   int min_value = arr[start];
@@ -77,7 +95,7 @@ int find_min_subarray(int *arr, size_t start, size_t end, size_t &index) {
   return min_value;
 }
 
-int* square_selection_sort(int* input_array, size_t size) {
+int* square_selection_sort_copy(int* input_array, size_t size) {
   int* sorted = new int[size];
   int* array = new int[size];
   for(size_t i = 0; i< size; i++) {
@@ -122,8 +140,9 @@ int* square_selection_sort(int* input_array, size_t size) {
     //step 4: move minimum to sorted, remove it from starting array, replace with new minimum
     sorted[i] = global_min;
 
-    size_t index_of_min = min_index_in_block[global_min_index];
-    array[index_of_min] = INT_MAX; // replace with max possible value - it will not become new minimum
+    size_t index_of_min = min_index_in_block[global_min_index]; // index in large array
+
+    //array[index_of_min] = INT_MAX; // replace with max possible value - it will not become new minimum
 
     size_t block_start = global_min_index*block_size;
     size_t current_block_size = block_size;
@@ -131,7 +150,8 @@ int* square_selection_sort(int* input_array, size_t size) {
       current_block_size = size - (num_blocks-1)*block_size;
     }
     size_t index;
-    min_in_block[global_min_index] = find_min(array + block_start, current_block_size, index );
+    min_in_block[global_min_index] = find_next_min(array + block_start, current_block_size,
+                                                    global_min, index_of_min, index );
     min_index_in_block[global_min_index] = index + block_start;
 
     //print_array(array, size);
@@ -144,6 +164,74 @@ int* square_selection_sort(int* input_array, size_t size) {
   delete [] array;
   return sorted;
 }
+
+
+int* square_selection_sort(int* array, size_t size) {
+  int* sorted = new int[size];
+
+  //step 1: divide into blocks of same size
+  size_t block_size = std::round(std::sqrt(size)); // |B_i| in slides
+  size_t num_blocks = std::ceil(std::sqrt(size)); // m in slides
+
+  assert( size <= num_blocks*block_size); //original array should fit in all blocks, maybe with last block not full
+  assert(num_blocks >= block_size); // num_blocks and block_size either equal (for full square), or num_blocks larger by 1
+  assert(num_blocks - block_size <= 1);
+
+  int* min_in_block = new int[num_blocks]; // g_1, ..., g_m in slides
+  size_t* min_index_in_block = new size_t[num_blocks];
+
+  // step2: find minimums in blocks
+  for (size_t i = 0; i < num_blocks; i++) {
+    //int min_value = ???
+    size_t block_start = i*block_size;
+    size_t current_block_size = block_size;
+    if (i == num_blocks-1) {
+      current_block_size = size - (num_blocks-1)*block_size;
+    }
+
+    size_t index;
+    min_in_block[i] = find_min(array + block_start, current_block_size, index );
+    //std::cout<<"in block "<<i<<" min is "<<min_in_block[i]<<" and index is "<<index<<std::endl;
+    min_index_in_block[i] = index + block_start;
+  }
+  //print_array(min_in_block, num_blocks);
+  //print_array(min_index_in_block, num_blocks);
+
+  for (size_t i = 0; i < size; i++ ) { // step 5 - repeat steps 3 and 4 for all items
+    //step 3: find min of mins
+    size_t global_min_index; // i in slides
+    int global_min = find_min(min_in_block, num_blocks, global_min_index); // g_i in slides
+
+    //std::cout<<"global min = "<<global_min<<" in position "<<global_min_index<<std::endl;
+
+    //step 4: move minimum to sorted, remove it from starting array, replace with new minimum
+    sorted[i] = global_min;
+
+    size_t index_of_min = min_index_in_block[global_min_index]; // index in large array
+
+    //array[index_of_min] = INT_MAX; // replace with max possible value - it will not become new minimum
+
+    size_t block_start = global_min_index*block_size;
+    size_t current_block_size = block_size;
+    if (global_min_index == num_blocks-1) {
+      current_block_size = size - (num_blocks-1)*block_size;
+    }
+    size_t index;
+    min_in_block[global_min_index] = find_next_min(array + block_start, current_block_size,
+                                                    global_min, index_of_min, index );
+    min_index_in_block[global_min_index] = index + block_start;
+
+    //print_array(array, size);
+    //print_array(sorted, size);
+  }
+
+
+
+  //print_array(sorted, size);
+  return sorted;
+}
+
+
 
 void array_syntax_demo(int array2[]) {
   assert(array2 + 1 == &array2[1]);
