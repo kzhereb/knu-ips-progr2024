d0d21b8eaf2cd775e4b5c4a5de8c779f2dec05d7
Kostiantyn Zhereb <zhereb@gmail.com>
2024-04-18 14:08:10 +0300

    Implemented method for adding edge
    Uses bitwise operations - bitwise OR and left shift to set particular
    bit to 1
    Bits are counted from least significant

--- src/lesson_2024_04_18_bitvector.cpp
+++ src/lesson_2024_04_18_bitvector.cpp
@@ -7,11 +7,17 @@
 
 #include <iostream>
 #include <vector>
+#include <bitset>
 #include <cassert>
 
 namespace lesson_2024_04_18_bitvector {
 
 const size_t MAX_BITVECTOR_SIZE = sizeof(unsigned) *8;
+
+
+void set_bit(unsigned& row, size_t position) {
+  row = row | (1 << position);
+}
 
 struct BinaryVectorGraph {
   std::vector<unsigned> edges_matrix; // in binary format, each row of matrix stored as unsigned
@@ -20,8 +26,17 @@ struct BinaryVectorGraph {
     assert(vertex_count <= MAX_BITVECTOR_SIZE);
     assert(edges_matrix.size() == vertex_count);
   }
+
+  void add_edge(size_t from, size_t to) {
+    assert(from < edges_matrix.size());
+    assert(to < edges_matrix.size());
+
+    set_bit(edges_matrix[from], to);
+  }
 };
 
+
+//has_edge
 
 
 void demo_number_representation() {
@@ -36,9 +51,55 @@ void demo_number_representation() {
   std::cout<< "octal="<<octal<<std::endl;
 }
 
+void demo_binary_operations() {
+  unsigned input = 0b110;
+  unsigned expected = 0b1100; // NOT 0b101
+  unsigned actual = input<<1;
+
+  std::cout<<"expected = " <<std::bitset<8>(expected)<<std::endl;
+  std::cout<<"actual = " <<std::bitset<8>(actual)<<std::endl;
+  assert(expected == actual);
+
+  input = 0b1001;
+  expected = 0b1101;
+  actual = input | 0b0100;
+  std::cout<<"expected = " <<std::bitset<8>(expected)<<std::endl;
+  std::cout<<"actual = " <<std::bitset<8>(actual)<<std::endl;
+  assert(expected == actual);
+
+  input = 2;
+  expected = 0b0100;
+  actual = 1 << input ;
+  std::cout<<"expected = " <<std::bitset<8>(expected)<<std::endl;
+  std::cout<<"actual = " <<std::bitset<8>(actual)<<std::endl;
+  assert(expected == actual);
+
+  input = 0b1001;
+  size_t position = 2;
+  expected = 0b1101;
+  actual = input | ( 1 << position);
+  std::cout<<"expected = " <<std::bitset<8>(expected)<<std::endl;
+  std::cout<<"actual = " <<std::bitset<8>(actual)<<std::endl;
+  assert(expected == actual);
+
+  input = 0b1001;
+  position = 2;
+  expected = 0b1101;
+  actual = input;
+  set_bit(actual, position);
+  std::cout<<"expected = " <<std::bitset<8>(expected)<<std::endl;
+  std::cout<<"actual = " <<std::bitset<8>(actual)<<std::endl;
+  assert(expected == actual);
+
+
+
+}
+
 int main() {
   //demo_number_representation();
+  demo_binary_operations();
   BinaryVectorGraph graph(5);
+
   return 0;
 }
 
