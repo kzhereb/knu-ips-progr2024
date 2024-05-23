cad5274d4213828b02defb668cfd592a4b78bb0f
Kostiantyn Zhereb <zhereb@gmail.com>
2024-05-23 15:08:29 +0300

    Added spaces between tokens in output
    This is needed to distinguish multiple numbers from single number with
    more digits

--- src/lesson_2024_05_23_expression_postfix.cpp
+++ src/lesson_2024_05_23_expression_postfix.cpp
@@ -13,6 +13,10 @@
 namespace lesson_2024_05_23_expression_postfix {
 
 
+bool is_digit(char current) {
+  return current <= '9' && current >= '0';
+}
+
 bool is_operator(char current) {
   if (current == '-' || current == '/') { return true;}
   return false;
@@ -23,55 +27,71 @@ bool less_precedence(char operator1, char operator2) {
   return false;
 }
 
+void pop_stack_to_result(char from_stack, std::stack<char> &operator_stack,
+    std::string &result) {
+  // greater precedence, or same precedence and left associative
+  operator_stack.pop();
+  result += from_stack;
+  result += ' ';
+}
+
 std::string infix_to_postfix(std::string expression) {
   std::string result;
   std::stack<char> operator_stack;
+  bool is_number = false;
 
   for (char current: expression) {
-    if (current <= '9' && current >= '0') {
+    if (is_digit(current)) {
       result += current;
-    } else if (is_operator(current)) {
-      while (!operator_stack.empty()) {
-        char from_stack = operator_stack.top();
-        if (is_operator(from_stack)) {
-          if (less_precedence(from_stack, current)) {
-            break; //less precedence
-          } else { // greater precedence, or same precedence and left associative
-            operator_stack.pop();
-            result += from_stack;
-          }
-        } else if (from_stack == '(') {
-          break; // no more operators for result
-        }
-      } // while there are suitable operators on stack
-      operator_stack.push(current);
-    } else if (current == '(') {
-      operator_stack.push(current);
-    } else if (current == ')') {
-      while (!operator_stack.empty()) {
-        char from_stack = operator_stack.top();
-        if (from_stack != '(') {
-          operator_stack.pop();
-          result += from_stack;
-        } else {
-          break;
-        }
-      } // while operator on stack is not (
-      if (operator_stack.empty()) {
-        return "error: ')' without '('";
+      is_number = true;
+    } else { // not digit
+      if (is_number) {
+        result += ' ';
+        is_number = false;
       }
-      assert(operator_stack.top() == '(');
-      operator_stack.pop(); // discard matching (
-    } // if current
-  } // for current: input
+      if (is_operator(current)) {
+        while (!operator_stack.empty()) {
+          char from_stack = operator_stack.top();
+          if (is_operator(from_stack)) {
+            if (less_precedence(from_stack, current)) {
+              break; //less precedence
+            } else { // greater precedence, or same precedence and left associative
+              pop_stack_to_result(from_stack, operator_stack, result);
+            }
+          } else if (from_stack == '(') {
+            break; // no more operators for result
+          }
+        } // while there are suitable operators on stack
+        operator_stack.push(current);
+      } else if (current == '(') {
+        operator_stack.push(current);
+      } else if (current == ')') {
+          while (!operator_stack.empty()) {
+            char from_stack = operator_stack.top();
+            if (from_stack != '(') {
+              pop_stack_to_result(from_stack, operator_stack, result);
+            } else {
+              break;
+            }
+          } // while operator on stack is not (
+          if (operator_stack.empty()) {
+            return "error: ')' without '('";
+          }
+          assert(operator_stack.top() == '(');
+          operator_stack.pop(); // discard matching (
+      } // if current
+    } // if current is digit
 
+  } // for current: input
+  if (is_number) {
+    result += ' ';
+  }
   while(!operator_stack.empty()) {
     char from_stack = operator_stack.top();
     if (from_stack == '(') {
       return "error: '(' without ')'";
     }
-    operator_stack.pop();
-    result += from_stack;
+    pop_stack_to_result(from_stack, operator_stack, result);
   }
 
   return result;
@@ -96,6 +116,7 @@ int main() {
   check_expression("1-2/3");
   check_expression(")");
   check_expression("(");
+  check_expression("11-22/33");
 
 
   return 0;
